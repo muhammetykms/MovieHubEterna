049d94ccd41b65d9864db36b2ddfdae44cfc8926
Muhammet Yıkmış <muhammetyikmis@192.168.1.15>
2025-02-07 00:06:16 +0300

    App file updated

--- App.tsx
+++ App.tsx
@@ -1,17 +1,32 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {FavoriteMoviesProvider} from './src/context/FavoriteMoviesContext'; // Import context
 import TabNavigator from './src/navigation/TabNavigator'; // Diğer navigatörünüz
+import MovieDetailScreen from './src/screens/MovieDetailScreen/MovieDetailScreen';
+import CastDetailScreen from './src/screens/CastDetailScreen/CastDetailScreen';
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{headerShown: false}}>
-        <Stack.Screen name="Tab" component={TabNavigator} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <FavoriteMoviesProvider>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{headerShown: false}}>
+          <Stack.Screen name="Tab" component={TabNavigator} />
+          <Stack.Screen
+            name="Details"
+            component={MovieDetailScreen}
+            options={{headerShown: false}}
+          />
+          <Stack.Screen
+            name="CastDetail"
+            component={CastDetailScreen}
+            options={{headerShown: false}}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </FavoriteMoviesProvider>
   );
 };
 
