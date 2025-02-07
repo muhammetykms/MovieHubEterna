57566076dd17cba989683b689e847ccd807590f8
Muhammet Yıkmış <muhammetyikmis@192.168.1.15>
2025-02-07 23:27:27 +0300

    edited

--- App.tsx
+++ App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {Provider} from 'react-redux'; // Redux Provider'ı import ediyoruz
-import {store} from './src/redux/store'; // Redux store'u import ediyoruz
-import {ThemeProvider} from './src/theme/ThemeProvider'; // Tema sağlayıcıyı import ediyoruz
-import TabNavigator from './src/navigation/TabNavigator'; // Ana navigasyon
+import {Provider} from 'react-redux';
+import {store} from './src/redux/store';
+import {ThemeProvider} from './src/theme/ThemeProvider';
+import TabNavigator from './src/navigation/TabNavigator';
 import MovieDetailScreen from './src/screens/MovieDetailScreen/MovieDetailScreen';
 import CastDetailScreen from './src/screens/CastDetailScreen/CastDetailScreen';
 
@@ -13,9 +13,7 @@ const Stack = createNativeStackNavigator();
 const App = () => {
   return (
     <Provider store={store}>
-      {/* Redux store'u sağlıyoruz */}
       <ThemeProvider>
-        {/* Tema sağlayıcıyı sarıyoruz */}
         <NavigationContainer>
           <Stack.Navigator screenOptions={{headerShown: false}}>
             <Stack.Screen name="Tab" component={TabNavigator} />
