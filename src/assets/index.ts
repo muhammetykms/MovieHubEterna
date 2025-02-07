b882ee1a2309dd2e7380b1a0f47c2d97a34edf33
Muhammet Yıkmış <muhammetyikmis@192.168.1.15>
2025-02-07 10:57:20 +0300

    icons added to assets

---
+++ src/assets/index.ts
@@ -0,0 +1,9 @@
+const IMAGES = {
+  FAVORITE: require('./favorite.png'),
+  HOME: require('./home.png'),
+  SEARCH: require('./search.png'),
+  PROFILE: require('./user.png'),
+  ERROR: require('./error.png'),
+};
+
+export default IMAGES;
