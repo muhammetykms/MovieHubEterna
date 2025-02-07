a52c38eef1c9878963f655bd93021d28e6ec7bbf
Muhammet Yıkmış <muhammetyikmis@192.168.1.15>
2025-02-07 21:09:24 +0300

    try-catch management

--- src/services/MovieService.ts
+++ src/services/MovieService.ts
@@ -2,18 +2,27 @@
 // ve MovieDTO ve CastDTO türlerine dönüştürerek döner.
 
 import {MovieDTO, CastDTO} from '../data/MoviesDTO';
-
 const mockData = require('../data/mockData.json');
 
 export const fetchMovies = (): MovieDTO[] => {
-  return mockData.movies.map(
-    (movie: (typeof mockData.movies)[0]): MovieDTO => ({
-      ...movie,
-      casts: movie.casts.map(
-        (cast: (typeof movie.casts)[0]): CastDTO => ({
-          ...cast,
-        }),
-      ),
-    }),
-  );
+  try {
+    if (!mockData || !mockData.movies) {
+      throw new Error('Movies data is missing');
+    }
+
+    return mockData.movies.map(
+      (movie: (typeof mockData.movies)[0]): MovieDTO => ({
+        ...movie,
+        casts: movie.casts.map(
+          (cast: (typeof movie.casts)[0]): CastDTO => ({
+            ...cast,
+          }),
+        ),
+      }),
+    );
+  } catch (error) {
+    console.error('Error fetching movies:', error);
+    // Hata durumunda boş bir dizi dönebiliriz veya uygun bir hata mesajı verebiliriz
+    return [];
+  }
 };
