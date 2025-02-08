9243a8fdb23e3e004ebdb576fc508b8ef4e6052a
Muhammet Yıkmış <muhammetyikmis@192.168.1.15>
2025-02-08 19:31:05 +0300

    unnecessary file deleted

--- src/utils/helpers.ts
+++
@@ -1,9 +0,0 @@
-import {MovieDTO} from '../data/MoviesDTO';
-
-export const groupMoviesInPairs = (movies: MovieDTO[]) => {
-  const groupedMovies = [];
-  for (let i = 0; i < movies.length; i += 2) {
-    groupedMovies.push(movies.slice(i, i + 2));
-  }
-  return groupedMovies;
-};
